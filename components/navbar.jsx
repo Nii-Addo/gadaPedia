7b271857807439359a786297e229090b4b904a4a
godfather <ankamahgodson@yahoo.com>
2021-11-26 16:42:05 +0000

    fix link issues

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import SearchBar from './searchbar';
+import Link from 'next/link';
 
 const Navbar = () => {
   return (
@@ -14,14 +15,14 @@ const Navbar = () => {
                 className='no-underline text-black text-2xl md:text-4xl md:border-b-2 md:border-white hover:border-b-4 hover:border-black'
                >
                 Gitpedia
-            </p></Link>
+            </p></Link> 
             </div>
             <div className='invicible md:visible absolute md:relative right-0 py-1 md:py-4 w-1/2 md:mb-0 md:w-1/3 pr-5 md:pr-0 flex-shrink-0'>
               <SearchBar />
             </div>
-            <div class="block lg:hidden m-4">
-                <button class="flex items-center px-3 py-2 border rounded text-black-200 border-black-400 hover:text-black hover:border-white">
-                  <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+            <div className="block lg:hidden m-4">
+                <button className="flex items-center px-3 py-2 border rounded text-black-200 border-black-400 hover:text-black hover:border-white">
+                  <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
                 </button>
               </div>
             <div className='hidden md:flex md:justify-center md:space-x-4 md:w-1/3 absolute md:right-0 md:h-full'>
@@ -38,7 +39,7 @@ const Navbar = () => {
               </a>
 
               <a
-                href='#'
+                href='/'
                 className='text-black md:border-b-2 md:border-white hover:border-b-4 hover:border-black px-3 py-6 text-md font-medium'>
                 Login
               </a>
